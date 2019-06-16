6f82c5376c351d8d6dc95117d983686c914ac3ae
DouglasAntunes <DouglasAntunes@users.noreply.github.com>
2019-06-16 18:12:39 -0300

    Initial Implementation

---
+++ BotFalandaum/BotFalandaum/Sound.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BotFalandaum
+{
+    class Sound
+    {
+        string name;
+        int weight;
+        int partDelay;
+        byte[] buffer;
+
+        //igual ao createSound()
+        public Sound(string name, int weight, int partDelay)
+        {
+            this.Name = name;
+            this.Weight = weight;
+            this.PartDelay = partDelay;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public int Weight { get => weight; set => weight = value; }
+        public int PartDelay { get => partDelay; set => partDelay = value; }
+        public byte[] Buffer { get => buffer; set => buffer = value; }
+
+        public void load()
+        {
+            byte[] bytes = File.ReadAllBytes(null);
+            using (FileStream fs = new FileStream(null, FileMode.Open, FileAccess.Read))
+            {
+
+                MemoryStream ms = new MemoryStream(bytes.Length);
+                BinaryReader b = new BinaryReader(fs);
+
+                while (fs.Read(null, 0, sizeof(Int16)) > 0)
+                {
+                    ms.Write(Buffer, 0, bytes.Length);
+                }
+            }
+        }
+    }
+}
---
+++ BotFalandaum/BotFalandaum/SoundCollection.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BotFalandaum
+{
+    class SoundCollection
+    {
+        string prefix;
+        string[] commands;
+        Sound[] sounds;
+        int soundRange;
+
+        public string Prefix { get => prefix; set => prefix = value; }
+        public string[] Commands { get => commands; set => commands = value; }
+        public Sound[] Sounds { get => sounds; set => sounds = value; }
+        public int SoundRange { get => soundRange; set => soundRange = value; }
+
+        public void Load()
+        {
+            foreach (Sound s in Sounds)
+            {
+                this.SoundRange += s.Weight;
+                s.load();
+            }
+
+        }
+
+        public Sound Random()
+        {
+            int j;
+            int number = randomRange(0, soundRange);
+
+            foreach (Sound s in sounds)
+            {
+                j += s.Weight;
+
+                if (number < j)
+                {
+                    return s;
+                }
+            }
+            return null;
+        }
+
+        private int randomRange(int min, int max)
+        {
+            //Criar Seed
+            //Retornar 
+            return 0;
+        }
+    }
+}
